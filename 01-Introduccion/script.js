4c4ea620c5ed9ac92e2a9b18659998200f175a35
Dianakoch <ortizchavezdianakaren@gmail.com>
2025-05-02 11:35:00 -0600

    second commit

---
+++ 01-Introduccion/script.js
@@ -0,0 +1,29 @@
+/*
+comentar varias lineas
+*/
+//comentar solo una linea
+
+
+// pregunta el usuario
+const nombre = prompt("cual es tu nombre");
+
+//toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo js`;
+
+//asigan un valor a variable
+const producto = 'monito 24 pulgadas';
+
+console.error("horrores");
+console.warn("eso no esta permitido");
+
+///////////////////////////
+console.log("hola");
+console.log("mundo") 
+
+function hola(){
+    console.log("hola");//siempre dejar un espacio
+}
+
+//descargar ESlint y node js
+
+
